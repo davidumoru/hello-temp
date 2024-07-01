cbb8031475928b76fb8f0a20fff3a5b1422b95cd
David Umoru <umorudavido@gmail.com>
2024-07-01 16:16:15 +0100

    Add API endpoint for retrieving location, weather data

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 const express = require("express");
+const axios = require("axios");
+const dotenv = require("dotenv");
+
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.get("/api/hello", async (req, res) => {
+  const name = req.query.visitor_name || "Guest";
+  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
+  try {
+    // Get location data
+    const locationResponse = await axios.get(`http://ip-api.com/json/${ip}`);
+    const locationData = locationResponse.data;
+    const city = locationData.city || "Unknown city";
+
+    // Get weather data
+    const weatherApiKey = process.env.TEMP_API;
+    const weatherResponse = await axios.get(
+      `http://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${city}`
+    );
+    const weatherData = weatherResponse.data;
+    const temperature = weatherData.current.temp_c;
+
+    res.json({
+      client_ip: ip,
+      location: city,
+      greeting: `Hello, ${name}!, the temperature is ${temperature} degrees Celsius in ${city}`,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
